3d443b35933e08f9685b03aad74b976aa273b626
faly9 <randriantsoafaly8@gmail.com>
2025-12-04 07:44:26 +0000

    h

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -159,7 +159,7 @@ export default function Login() {
             className="text-4xl font-bold mb-6 text-center"
             style={{ color: "var(--text-primary)" }}
           >
-            Connexion faly
+            Connexion 
           </h1>
 
           {message && (
