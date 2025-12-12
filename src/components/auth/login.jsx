fb31a0b40ea7af1eb3f09edeb2c957716b29836b
faly9 <randriantsoafaly8@gmail.com>
2025-12-12 11:15:03 +0000

    test

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -156,7 +156,7 @@ export default function Login() {
             className="text-4xl font-bold mb-6 text-center"
             style={{ color: "var(--text-primary)" }}
           >
-            Connexion pour tous
+            Connexion pour tout le monde
           </h1>
 
           {message && (
